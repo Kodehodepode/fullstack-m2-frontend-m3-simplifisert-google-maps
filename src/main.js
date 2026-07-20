23c6cbd5292b4e3feb7800c3378d5263ec7e3e82
Kodehodepode <kare.kodehode@gmail.com>
2026-07-20 15:05:31 +0200

    Søkefunksjon

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import markerIconUrl from 'leaflet/dist/images/marker-icon.png';
 import markerIconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
+const geoApifyURL = "https://api.geoapify.com/v1/geocode/search";
+const geoApifyKey = import.meta.env.VITE_GeoApify_KEY
+
+
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: markerIconRetinaUrl,
@@ -24,7 +28,29 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+/*
 // Add a Sample Marker
 L.marker([51.505, -0.09]).addTo(map)
   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
   .openPopup();
+*/
+
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+searchButton.addEventListener("click", async (event) => {
+  const url = new URL(geoApifyURL);
+
+  const params = {
+    apiKey: geoApifyKey,
+    text: searchInput.value
+  };
+  url.search = new URLSearchParams(params).toString();
+
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    
+  const data = await response.json();
+  const coordinates = data.features[0].geometry.coordinates.toReversed();
+
+  map.setView(coordinates, 13);
+});
